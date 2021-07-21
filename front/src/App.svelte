e332b4ce11eb4c5a1362f0cec54030def958f67b
Alejandro Alonso <alejandroalonsofernandez@gmail.com>
2021-07-21 10:10:06 +0200

    wip

--- front/src/App.svelte
+++ front/src/App.svelte
@@ -7,155 +7,18 @@
 
 
 <script lang="ts">
-	// export let name: string;
-  let dimension = 'lines';
-
-	import * as Pancake from '@sveltejs/pancake';
-	import * as d3 from 'd3-hierarchy';
-	import { tweened } from 'svelte/motion';
-	import * as eases from 'svelte/easing';
-	import { fade } from 'svelte/transition';
-	import * as yootils from 'yootils';
 	import Treemap from './Treemap.svelte';
-	import data from './data.js';
-
-  const treemap = d3.treemap();
-
-  const extents = tweened(undefined, {
-    easing: eases.cubicOut,
-    duration: 600
-  });
-
-  let selected = undefined;
-  let root = undefined;
-
-  $: $extents = {
-    x1: selected ? selected.x0 : 0,
-    x2: selected ? selected.x1 : 0,
-    y1: selected ? selected.y1 : 0,
-    y2: selected ? selected.y0 : 0
-  };
-
-  const hierarchyData = {
-    name: "",
-    children: {}
-  };
-
-  function breadcrumbs(node) {
-    const crumbs = [];
-    while (node) {
-      crumbs.unshift(node.data.name)
-      node = node.parent;
-    }
-		const b = crumbs.join('/');
-    return b.startsWith('/') ? b.substring(1) : b;
-  };
-
-  function is_visible(a, b) {
-    while (b) {
-      if (a.parent === b) return true;
-      b = b.parent;
-    }
-
-    return false;
-  };
-
-  async function select(node) {
-    while (node.parent && node.parent !== selected) {
-      node = node.parent;
-    }
-    if (node && node.children) selected = node;
-    selected = node;
-  };
-
-  function flattenData(hierarchyData) {
-    let keys = Object.keys(hierarchyData);
-    if (hierarchyData.children === undefined) {
-      return hierarchyData;
-    }
-    else {
-      const children = [];
-      Object.keys(hierarchyData.children).forEach((key) => {
-        children.push(
-          {
-            ...flattenData(hierarchyData.children[key]),
-            name: key
-          }
-        );
-      });
-
-      hierarchyData.children = children;
-      return hierarchyData;
-    }
-  }
 
 	async function getProjectData() {
 		const res = await fetch(`http://localhost:8000/files`);
 		const json = await res.json();
 
-    const maxLines = json.files.reduce((prev, current) => (prev.lines > current.lines) ? prev : current).lines;
-    const minLines = maxLines * .10;
-    const maxRating = json.files.reduce((prev, current) => (prev.rating > current.rating) ? prev : current).rating;
-    const minRating = maxRating * .10;
-
-		if (res.ok) {
-      json.files.forEach((f) => {
-        let path = f.directory  ? f.directory.split("/") : [];
-        if (f.isDirectory) {
-          path = path.concat([f.name])
-        }
-
-        let d = hierarchyData;
-        path.forEach((p) => {
-          if(!d.children) {
-            d.children = {};
-          }
-          if (!d.children[p]) {
-            d.children[p] = {};
-          }
-          d = d.children[p];
-          if(!d.children) {
-            d.children = {};
-          }
-        });
-
-        if (!f.isDirectory) {
-          d.children[f.name] = {
-            lines: f.lines,
-            rating: f.rating
-          };
-        } else{
-          d.lines = f.lines;
-          d.rating = f.rating;
-        }
-      });
-
-      flattenData(hierarchyData);
-
-      const hierarchy = d3.hierarchy(hierarchyData)
-        .sum(d => d.lines)
-        .sort((a, b) => b.lines - a.lines)
-
-      root = treemap(hierarchy);
-      selected = root;
-
+    if (res.ok) {
 			return json;
 		} else {
 			throw new Error(json);
 		}
 	}
-
-  function onChangeDimension(event) {
-		const val = event.currentTarget.value;
-    const hierarchy = d3.hierarchy(hierarchyData)
-      .sum(d => d[val])
-      .sort((a, b) => b[val] - a[val])
-
-    root = treemap(hierarchy);
-    selected = root;
-
-	}
-
 </script>
 
 <main>
@@ -167,39 +30,11 @@
     </a>
     </h1>
 
-    <label>
-      <input type=radio on:change={onChangeDimension} bind:group={dimension} name="dimension" value={'lines'}>
-      lines
-    </label>
-    <label>
-      <input type=radio on:change={onChangeDimension} bind:group={dimension} name="dimension" value={'rating'}>
-      rating
-    </label>
-
-    <button class="breadcrumbs" disabled="{!selected.parent}" on:click="{() => selected = selected.parent}">
-      {breadcrumbs(selected)}
-    </button>
-
     <div class="chart">
-      <Pancake.Chart x1={$extents.x1} x2={$extents.x2} y1={$extents.y1} y2={$extents.y2}>
-        <Treemap {root} let:node>
-          {#if is_visible(node, selected)}
-            <div
-              transition:fade={{duration:400}}
-              class="node"
-              class:leaf={!node.children}
-              on:click="{() => select(node)}"
-            >
-              <div class="contents">
-                <strong>{node.data.name}</strong>
-                <span>{yootils.commas(node.value)}</span>
-              </div>
-            </div>
-          {/if}
-        </Treemap>
-      </Pancake.Chart>
+      <Treemap data={projectInfo}/>
     </div>
 
+<!--
     <div class="analytics">
       <ul>
         {#if selected.data.lines != undefined}
@@ -210,6 +45,7 @@
         {/if}
       </ul>
     </div>
+-->
 
 <!--
     <ul class="uk-list uk-list-striped">
@@ -240,6 +76,25 @@
 		}
 	}
 
+	.chart :global(div) {
+		font: 10px sans-serif;
+		background-color: steelblue;
+		text-align: right;
+		padding: 3px;
+		margin: 1px;
+		color: white;
+	}
+
+	.chart {
+    float: left;
+		width: calc(80% + 2px);
+		height: 400px;
+		padding: 0;
+		margin: 0 -1px 36px -1px;
+		overflow: hidden;
+	}
+
+/* 
   .breadcrumbs {
 		width: 100%;
 		padding: 0.3rem 0.4rem;
@@ -256,14 +111,6 @@
 		cursor: default;
 	}
 
-	.chart {
-    float: left;
-		width: calc(80% + 2px);
-		height: 400px;
-		padding: 0;
-		margin: 0 -1px 36px -1px;
-		overflow: hidden;
-	}
 
   .analytics {
     width: calc(20% - 2px);
@@ -304,7 +151,7 @@
 		font-size: 12px;
 		white-space: nowrap;
 		line-height: 1;
-	}
+	} */
 
   /* .uk-list {
     clear: both;
--- front/src/Treemap.svelte
+++ front/src/Treemap.svelte
@@ -1,20 +1,195 @@
 <script>
-	import TreemapNode from './TreemapNode.svelte';
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
 
-	export let root;
+  export let data;
+  let el;
+
+  const hierarchyData = {
+    name: ".",
+    children: {}
+  };
+
+	onMount(async () => {
+    const width = 400;
+    const height = 200;
+
+    const format = d3.format(",d");
+
+    const name = d => d.ancestors().reverse().map(d => d.data.name).join("/")
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
+
+    data.files.forEach((f) => {
+      let path = f.directory  ? f.directory.split("/") : [];
+      if (f.isDirectory) {
+        path = path.concat([f.name])
+      }
+
+      let d = hierarchyData;
+      path.forEach((p) => {
+        if(!d.children) {
+          d.children = {};
+        }
+        if (!d.children[p]) {
+          d.children[p] = {};
+        }
+        d = d.children[p];
+        if(!d.children) {
+          d.children = {};
+        }
+      });
+
+      if (!f.isDirectory) {
+        d.children[f.name] = {
+          lines: f.lines,
+          rating: f.rating
+        };
+      }
+    });
+
+    flattenData(hierarchyData);
+
+    console.log("hierarchyData", hierarchyData)
+
+    function tile(node, x0, y0, x1, y1) {
+      d3.treemapBinary(node, 0, 0, width, height);
+      for (const child of node.children) {
+        child.x0 = x0 + child.x0 / width * (x1 - x0);
+        child.x1 = x0 + child.x1 / width * (x1 - x0);
+        child.y0 = y0 + child.y0 / height * (y1 - y0);
+        child.y1 = y0 + child.y1 / height * (y1 - y0);
+      }
+    }
+
+    const treemap = data => d3.treemap()
+        .tile(tile)
+      (d3.hierarchy(hierarchyData)
+        .sum(d => d.lines)
+        .sort((a, b) => b.lines - a.lines));
+
+    const x = d3.scaleLinear().rangeRound([0, width]);
+    const y = d3.scaleLinear().rangeRound([0, height]);
+
+    const svg = d3.select(el)
+        .attr("viewBox", [0.5, -30.5, width, height + 30])
+        .style("font", "4px sans-serif");
+
+    let group = svg.append("g")
+        .call(render, treemap(hierarchyData));
+
+    function render(group, root) {
+      const node = group
+        .selectAll("g")
+        .data(root.children.concat(root))
+        .join("g");
+
+      node.filter(d => d === root ? d.parent : d.children)
+          .attr("cursor", "pointer")
+          .on("click", (event, d) => d === root ? zoomout(root) : zoomin(d));
+
+      node.append("title")
+          .text(d => `${name(d)}\n${format(d.value)}`);
+
+      node.append("rect")
+          // .attr("id", d => uuidv4())
+          // .attr("fill", d => d === root ? "#fff" : d.children ? "#ccc" : "#ddd")
+          .attr("fill", d => {
+            while (d.depth > 1) {
+              d = d.parent;
+            }
+            return color(d.data.rating);
+          })
+          .attr("stroke", "#fff");
+
+      node.append("clipPath")
+        // .attr("id", d => uuidv4())
+        .append("use")
+        // .attr("xlink:href", d => d.leafUid.href);
+
+      node.append("text")
+          .attr("clip-path", d => d.clipUid)
+          .attr("font-weight", d => d === root ? "bold" : null)
+          .selectAll("tspan")
+          .data(d => (d === root ? name(d) : d.data.name).split(/(?=[A-Z][^A-Z])/g).concat(format(d.value)))
+          .join("tspan")
+          .attr("x", 3)
+          .attr("y", (d, i, nodes) => `${(i === nodes.length - 1) * 0.3 + 1.1 + i * 0.9}em`)
+          .attr("fill-opacity", (d, i, nodes) => i === nodes.length - 1 ? 0.7 : null)
+          .attr("font-weight", (d, i, nodes) => i === nodes.length - 1 ? "normal" : null)
+          .text(d => d);
+
+      group.call(position, root);
+    }
+
+    function position(group, root) {
+      group.selectAll("g")
+          .attr("transform", d => d === root ? `translate(0,-30)` : `translate(${x(d.x0)},${y(d.y0)})`)
+        .select("rect")
+          .attr("width", d => d === root ? width : x(d.x1) - x(d.x0))
+          .attr("height", d => d === root ? 30 : y(d.y1) - y(d.y0));
+    }
+
+    // When zooming in, draw the new nodes on top, and fade them in.
+    function zoomin(d) {
+      const group0 = group.attr("pointer-events", "none");
+      const group1 = group = svg.append("g").call(render, d);
+
+      x.domain([d.x0, d.x1]);
+      y.domain([d.y0, d.y1]);
+
+      svg.transition()
+          .duration(750)
+          .call(t => group0.transition(t).remove()
+            .call(position, d.parent))
+          .call(t => group1.transition(t)
+            .attrTween("opacity", () => d3.interpolate(0, 1))
+            .call(position, d));
+    }
+
+    // When zooming out, draw the old nodes on top, and fade them out.
+    function zoomout(d) {
+      const group0 = group.attr("pointer-events", "none");
+      const group1 = group = svg.insert("g", "*").call(render, d.parent);
+
+      x.domain([d.parent.x0, d.parent.x1]);
+      y.domain([d.parent.y0, d.parent.y1]);
+
+      svg.transition()
+          .duration(750)
+          .call(t => group0.transition(t).remove()
+            .attrTween("opacity", () => d3.interpolate(1, 0))
+            .call(position, d))
+          .call(t => group1.transition(t)
+            .call(position, d.parent));
+    }
+
+	});
+
+  function flattenData(hierarchyData) {
+    let keys = Object.keys(hierarchyData);
+    if (hierarchyData.children === undefined) {
+      return hierarchyData;
+    }
+    else {
+      const children = [];
+      Object.keys(hierarchyData.children).forEach((key) => {
+        children.push(
+          {
+            ...flattenData(hierarchyData.children[key]),
+            name: key
+          }
+        );
+      });
+
+      hierarchyData.children = children;
+      return hierarchyData;
+    }
+  }
+
 </script>
 
-<pancake-treemap>
-	<TreemapNode node={root} let:node>
-		<slot {node}></slot>
-	</TreemapNode>
-</pancake-treemap>
+
+<svg bind:this={el} class="chart"></svg>
 
 <style>
-	pancake-treemap {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		pointer-events: none;
-	}
 </style>
